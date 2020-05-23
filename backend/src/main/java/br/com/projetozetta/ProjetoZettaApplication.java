59240f229d4dbe4b5f1606fcb9fcdb3f171f4785
basilioruan <rmb@estudante.ufla.br>
2020-05-23 17:07:57 -0300

    criando projeto

---
+++ backend/src/main/java/br/com/projetozetta/ProjetoZettaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetozetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoZettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoZettaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/projetozetta/ProjetoZettaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetozetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
